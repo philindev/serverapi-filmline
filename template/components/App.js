f6ad2b472117bc0aa5be5cb9695d027cbc4efcfd
Masyru <mimoz200385@gmail.com>
2020-04-06 20:34:15 +1000

    base start

---
+++ template/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+export default class App  extends Component{
+	constructor(props){
+		super(props);
+		this.state = {}
+	}
+
+	render(){
+		return <h1>Hello<h1>
+	}
+}
