c7aae5ad499780f4b2a75a309b0d751c28c9aa08
Maksim from Windows 10 <mimoz200385@gmail.com>
2020-04-07 22:48:14 +1000

    Added NavBar to website and 2 images + favicon

--- server/frontend/components/Header.js
+++ server/frontend/components/Header.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Navbar, Button, Form, Nav, FormControl} from "react-bootstrap";
 
 export default class Header extends Component{
     constructor(props) {
@@ -15,6 +16,30 @@ export default class Header extends Component{
     }
 
     render() {
-        return(<h1>Header</h1>);
+        let icon = <img src="./frontend/image/popcorn.png" alt="LineFilms" style={{
+                                        height: "45px",
+                                        cursor: "pointer"
+                                    }}/>;
+        let menu = <img src="./frontend/image/menu.png" alt="LineFilms" className="ml-1 mr-2" style={{
+                                        height: "35px",
+                                        cursor: "pointer"
+                                    }}/>;
+
+        let content_block =
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">{icon}</Navbar.Brand>
+                <Nav className="mr-auto">
+                      <Nav.Link href="#home">Top-100</Nav.Link>
+                      <Nav.Link href="#features">Announced</Nav.Link>
+                </Nav>
+                <Form inline>
+                      <FormControl type="text" placeholder="Search" className="mr-sm-2 col-xl-lg-4" />
+                      <Button variant="outline-secondary" className="mr-2 xl-lg-2 xs-md-1">Search</Button>
+                      {menu}
+                      <button id="login">Auth</button>
+                </Form>
+              </Navbar>;
+
+        return(content_block);
     }
 }
